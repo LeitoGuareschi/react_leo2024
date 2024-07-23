49f4edcf0427b7c43ca8490b8bab1f36e348004f
LeitoGuareschi <166618109+LeitoGuareschi@users.noreply.github.com>
2024-07-22 22:01:23 -0300

    transporte.react

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>Diseñador por: Leonel Guareschi - 2024</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+    return(
+        <header>
+        <div class="holder">
+            <img src="img/logo.png" width="100" alt="trasportes x"/>
+            <h1>Transportes X</h1>
+
+        </div>
+    </header>
+    )
+}
+
+export default Header;
